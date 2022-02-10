df903eb69319ff2c30cc5acc25251310ecc96746
Nycto <James@RoundEights.com>
2022-02-10 07:25:57 -0800

    Refactor bounding box drawing

--- src/systems/render.nim
+++ src/systems/render.nim
@@ -3,25 +3,33 @@ import necsus, sdl2, sdl2/gfx, ../components, ../assets, vmath, collision
 proc drawLine(renderer: RendererPtr, p1, p2: Vec2; r, g, b: uint8) =
     renderer.lineRGBA(p1.x.int16, p1.y.int16, p2.x.int16, p2.y.int16, r, g, b, 255)
 
-proc renderBoundingBoxes(renderer: RendererPtr, bounds: Query[(Bounds, Position)]) =
+iterator lines(points: openarray[Vec2]): (Vec2, Vec2) =
+    ## Generates lines from a list of points
+    var firstPoint: Vec2
+    var recentPoint: Vec2
+    var isFirst: bool = true
+    for point in points:
+        if isFirst:
+            firstPoint = point
+            isFirst = false
+        else:
+            yield (recentPoint, point)
+        recentPoint = point
+    yield (recentPoint, firstPoint)
+
+proc reorient(point: Vec2, centroid: Position): Vec2 =
+    ## Adjusts the location of a point based on a centroid
+    (point + centroid.center).rotateAround(centroid.center, centroid.angle)
+
+proc renderBoundingBoxes(renderer: RendererPtr, bounds: Query[(Bounds, Position)]) {.used.} =
     ## Renders the given bounding boxes
     for (bound, pos) in bounds:
         case bound.kind
         of BoundsKind.Circle:
             renderer.circleRGBA(pos.center.x.int16, pos.center.y.int16, bound.radius.int16, 255, 0, 0, 255)
         of BoundsKind.Hull:
-            var firstPoint: Vec2
-            var recentPoint: Vec2
-            var isFirst: bool = true
-            for rawPoint in bound.points:
-                let point = (rawPoint + pos.center).rotateAround(pos.center, pos.angle)
-                if isFirst:
-                    firstPoint = point
-                    isFirst = false
-                else:
-                    renderer.drawLine(recentPoint, point, 255, 0, 0)
-                recentPoint = point
-            renderer.drawLine(recentPoint, firstPoint, 255, 0, 0)
+            for (a, b) in bound.points.lines:
+                renderer.drawLine(a.reorient(pos), b.reorient(pos), 255, 0, 0)
 
 proc renderer*(
     renderer: Shared[RendererPtr],
